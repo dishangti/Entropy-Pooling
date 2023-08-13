838579bd9212e81c56c3ea2dc82ca46d7e5bd0be
dishangti <16698219+dishangti@users.noreply.github.com>
2023-08-13 18:39:18 +0800

    Add RSI to author's 10y rate.

--- Meucci/S_CaseStudy_SingleRSI_r10y.m
+++ Meucci/S_CaseStudy_SingleRSI_r10y.m
@@ -5,69 +5,65 @@ clear all
 % at t^view = 1 year from today.
 
 %load the Calibrated Parameters
-load 'parameters_indices.mat'
+load 'parameters_rate.mat'
 mu = mu(1);
 theta = theta(1,1);
 sig2 = sig2(1,1);
 mu_LT = theta\mu;
 
 %load the simulated path for the 10y Government rate (this is equal to its shadow rate)
-load 'path_indices_daily.mat'
+load 'path_rate_daily.mat'
 
-start_time = 501;
-adjust_period = 10; % 90 periods
-end_time = 501 + adjust_period*50;    % 1800 days
+RSI = rsindex(X_path, 10);
 
-RSI_1 = rsindex(A300_path,10);
-A300_allPath = A300_path;
+start_time = 11;
+adjust_period = 10;
+end_time = 111;
+X_allPath = X_path;
+
+%%
 
 b_series = [];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %set the trading
 tau = 1/252;                        % trading frequency (daily)
-T_Hor = adjust_period / 252;%years             % effective future portfolio horizon   
+T_Hor = 10/252;%years                  % effective future portfolio horizon   
 tau_ = length([0:tau:T_Hor]);       % effective number of future tradings at any point in time
 n_ = 1;                             % number of risk drivers 
 n = 0;
 
 for time = start_time:adjust_period:end_time
     time
-    if exist("Posterior", "var")
-        mu = mu_x10y * theta;
-        %sig2 = sig2_1;
-    end
+    X_path = X_allPath(time:time+adjust_period - 1);
+    x0 = X_allPath(time);
 
     if exist('Posterior', 'var')
         b_series = [b_series b_MI_Bellman_post'];
+        mu = mu_x10y * theta;
     end
-    x0 = A300_allPath(time);
-    A300_path = A300_allPath(time:time+adjust_period - 1);
-    maxRSI = max(RSI_1(time - 10:time - 1));
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   
     %set the view at time 0
+    maxRSI = max(RSI(time - 10:time - 1));  
     t_view0 = adjust_period / 252;%years           % time of the view at time 0
     if maxRSI >= 70
-        mu_x10y = x0 * (1 - 0.15);
+        mu_x10y = x0 * (1 - 0.005);
     elseif maxRSI <= 30
-        mu_x10y = x0 * (1 + 0.12);
+        mu_x10y = x0 * (1 + 0.005);
     else
         mu_x10y = x0;
     end
     t = [0:tau:T_Hor t_view0]';         % monitoring times
     t_ = length(t);                     % number of monitoring times
-
+    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %set the parameters for optimization
     gamma = 10^-2;                      % risk aversion parameter    
-    eta = 2;                            % weight of the market impact of transaction
+    eta = 0.5;                          % weight of the market impact of transaction
     lambda = log(2)/20;                 % discount (half life 20*tau)
-    if exist('Posterior', 'var')
-        b_legacy = b_MI_Bellman_post(end);                       % legacy portfolio
-    else
-        b_legacy = 0
-    end
-
+    b_legacy = 0;                       % legacy portfolio
+    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %compute the prior at time 0
     Prior0 = MVOU_Prior(t, x0, theta, sig2, mu);
@@ -79,7 +75,7 @@ for time = start_time:adjust_period:end_time
     % Prior and Posterior Optimal exposure with Market Impcat of transaction.
     % SOLVING THE BELLMAN EQUATION
     
-    [b_MI_Bellman_prior, b_MI_Bellman_post] = BellmanEq_CS1(eta, gamma, lambda, tau, theta, mu, sig2, c2, b_legacy, A300_path, t_view0, mu_x10y);
+    [b_MI_Bellman_prior b_MI_Bellman_post] = BellmanEq_CS1(eta, gamma, lambda, tau, theta, mu, sig2, c2, b_legacy, X_path, t_view0, mu_x10y);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -112,7 +108,7 @@ for time = start_time:adjust_period:end_time
         %%The Prior
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %compute the prior
-        Prior = MVOU_Prior(t_roll, A300_path(i,:)', theta, sig2, mu);    
+        Prior = MVOU_Prior(t_roll, X_path(i,:)', theta, sig2, mu);    
         Mean_prior = reshape(Prior.mean(1:t_*n_),n_,t_)';        
        
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -123,10 +119,9 @@ for time = start_time:adjust_period:end_time
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %% Market impact: calculus of variation
-        ER = diff(Mean_prior(1:end-1));
-        l_t = - exp(-lambda*[0:1:length(ER)-1]').*diff(Mean_prior(1:end-1));
+        l_t = - exp(-lambda*[0:1:tau_-2]').*diff(Mean_prior(1:end-1));
         l_t(1) = l_t(1) - eta*c2*b_legacy_prior;
-        q_t = QuadraticMat_Vc(lambda,gamma,eta,Prior.cov,c2,length(ER),n_);
+        q_t = QuadraticMat_Vc(lambda,gamma,eta,Prior.cov,c2,tau_-1,n_);
         b_MI_Vc_prior_tmp = (2*q_t)\l_t;
         b_MI_Vc_prior(i,1:n_) = b_MI_Vc_prior_tmp(1:n_);
         b_legacy_prior = b_MI_Vc_prior(i,1:n_);
@@ -142,7 +137,7 @@ for time = start_time:adjust_period:end_time
         N_Meanviews = 1;%Number of views on expectations
         v_tmp = zeros(N_Meanviews,n_,t_);
         v_tmp(1,1,end) = 1;
-        mu_view = mu_x10y; 
+        mu_view(1,1) = mu_x10y; 
         v_mu = v_tmp(:,:);
         views = struct('N_Meanviews', N_Meanviews,'N_Covviews',[],'dimension',N(:),'monitoring_time',T(:),'v_mu',v_mu,'v_sig',NaN,'mu_view',mu_view,'sig2_view',[]);                    
         
@@ -159,28 +154,31 @@ for time = start_time:adjust_period:end_time
     
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %% Market impact: calculus of variation
-        ER = diff(Mean_post(1:end-1));
-        l_t = - exp(-lambda*[0:1:length(ER)-1]').*ER;
+        l_t = - exp(-lambda*[0:1:tau_-2]').*diff(Mean_post(1:end-1));
         l_t(1) = l_t(1) - eta*c2*b_legacy_post;
-        q_t = QuadraticMat_Vc(lambda,gamma,eta,Posterior.cov,c2,length(ER),n_);
+        q_t = QuadraticMat_Vc(lambda,gamma,eta,Posterior.cov,c2,tau_-1,n_);
         b_MI_Vc_post_tmp = (2*q_t)\l_t;
         b_MI_Vc_post(i,1:n_) = b_MI_Vc_post_tmp(1:n_);
         b_legacy_post = b_MI_Vc_post(i,1:n_);
        
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %% LongTerm and viewMean contributions of the optimal solution with views in absence of market impact of transactions 
-        b_NoMI_LongTerm(i,1:n_) = 2*theta/(gamma*sig2)/(1+exp(-theta*tau))*(1-(1+exp(theta*tau))/(1+exp(theta*(t_roll(end)))))*(mu/theta-A300_path(i,1));
-        b_NoMI_viewMean(i,1:n_) =  2*theta/(gamma*sig2)*exp(theta*tau)/(exp(theta*(t_roll(end)))-exp(-theta*t_roll(end)))*(mu_view-A300_path(i,1));
+        b_NoMI_LongTerm(i,1:n_) = 2*theta/(gamma*sig2)/(1+exp(-theta*tau))*(1-(1+exp(theta*tau))/(1+exp(theta*(t_roll(end)))))*(mu/theta-X_path(i,1));
+        b_NoMI_viewMean(i,1:n_) =  2*theta/(gamma*sig2)*exp(theta*tau)/(exp(theta*(t_roll(end)))-exp(-theta*t_roll(end)))*(mu_view-X_path(i,1));
     end
 end
 
 b_series = [b_series b_MI_Bellman_post'];
 t_series = [1:length(b_series)];
+length(t_series)
 yyaxis left
 ylabel('Exposure') 
 line(t_series, b_series)
+%origin
+load('CaseStudy_rate.mat', 'b_MI_Bellman_post');
+line(t_series, b_MI_Bellman_post(start_time:end_time + adjust_period - 1))
 yyaxis right
-ylabel('A300') 
-line(t_series, A300_allPath(start_time:end_time + adjust_period - 1))
+ylabel('10y rate') 
+line(t_series, X_allPath(start_time:end_time + adjust_period - 1))
 set(gca, 'XMinorGrid','on');
 grid on
